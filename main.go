65fd7e5697f8a30f5a3be7ef52565cc78e5b607d
Per Juchtmans <per@dubgeiser.net>
2023-09-16 11:12:03 +0200

    Go indents with TAB

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-    people, err := storage.GetPeople(10)
-    if err != nil {
-        panic(err)
-    }
-    for i := range people {
-        p:= people[i]
-        fmt.Println(p.ID, p.Name, p.Email)
-    }
+	people, err := storage.GetPeople(10)
+	if err != nil {
+		panic(err)
+	}
+	for i := range people {
+		p := people[i]
+		fmt.Println(p.ID, p.Name, p.Email)
+	}
 }
