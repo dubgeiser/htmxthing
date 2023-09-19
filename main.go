791b65daa6e37f24e8811dc3b9590283728de2ce
Per Juchtmans <per@dubgeiser.net>
2023-09-19 08:37:24 +0200

    I HAZ A WEBSITE
    First baby steps into building a web app.

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"htmxthing/storage"
+	"net/http"
 )
 
-func main() {
+func peopleList(w http.ResponseWriter, request *http.Request) {
 	people, err := storage.GetPeople(10)
 	if err != nil {
 		panic(err)
 	}
+	w.WriteHeader(http.StatusOK)
 	for i := range people {
 		p := people[i]
-		fmt.Println(p.ID, p.Name, p.Email)
+		w.Write([]byte(html.EscapeString(fmt.Sprint(p.ID) + ", " + p.Name + ", " + p.Email)))
 	}
+}
+
+func main() {
+	http.HandleFunc("/people", peopleList)
+	http.ListenAndServe(":8000", nil)
 }
